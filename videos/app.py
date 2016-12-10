f3ebf12c4e8d9c85dfd54527d4e8b21c707ec7ba
Andrei Duma <andrei.duma.dorian@gmail.com>
2016-12-10 04:54:16 +0200

    Added db support.

--- videos/app.py
+++ videos/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request, json
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, json, redirect, url_for
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -9,25 +13,38 @@ def index():
 
 @app.route('/new_video', methods=['POST'])
 def new_video():
-    return request.form['company_name']
+    company_name = request.form['company_name']
+    value = request.form['value']
+
+    _id = mongo.db.videos.insert({'company_name': company_name, 'value': value})
+    return redirect(url_for('get_video', _id=_id))
 
 
-@app.route('/videos/<url>', methods=['GET'])
-def get_video(url):
+@app.route('/videos/<_id>', methods=['GET'])
+def get_video(_id):
+    video = mongo.db.videos.find_one({'_id': ObjectId(_id)})
+
     captions = json.dumps([{
-        'text': 'J.P. Morgan',
-        'time': 1.2,
-        'duration': 4,
+        'text': 'At {} we care'.format(video['company_name']),
+        'time': 60 + 3,
+        'duration': 6,
         'x': 50,
-        'y': 40,
-        'size': 7,
+        'y': 50,
+        'size': 4,
     }, {
-        'text': 'Hugo Boss',
-        'time': 1.2,
-        'duration': 4,
+        'text': 'We give for a better future',
+        'time': 60 + 12,
+        'duration': 6,
         'x': 50,
-        'y': 70,
-        'size': 7,
+        'y': 50,
+        'size': 4,
+    }, {
+        'text': 'This year, our donations brought {} to research'.format(video['value']),
+        'time': 60 + 21,
+        'duration': 7,
+        'x': 50,
+        'y': 50,
+        'size': 4,
     }])
     return render_template('video.html', captions=captions)
 
